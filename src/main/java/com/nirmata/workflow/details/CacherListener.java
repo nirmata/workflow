1434dc42c25d802a88e7fce78c6dd85b7ca48cd3
randgalt <jordan@jordanzimmerman.com>
2014-09-20 09:09:57 -0500

    set FunctionalInterface

--- src/main/java/com/nirmata/workflow/details/CacherListener.java
+++ src/main/java/com/nirmata/workflow/details/CacherListener.java
@@ -2,6 +2,7 @@ package com.nirmata.workflow.details;
 
 import com.nirmata.workflow.details.internalmodels.DenormalizedWorkflowModel;
 
+@FunctionalInterface
 interface CacherListener
 {
     public void updateAndQueueTasks(Cacher cacher, DenormalizedWorkflowModel workflow);
