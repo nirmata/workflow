edebae601848187ee700d20860691df283be1bc9
randgalt <jordan@jordanzimmerman.com>
2014-09-21 02:41:55 -0500

    The custom edge wasn't needed

--- src/main/java/com/nirmata/workflow/details/RunnableTaskDagBuilder.java
+++ src/main/java/com/nirmata/workflow/details/RunnableTaskDagBuilder.java
@@ -1,13 +1,17 @@
 package com.nirmata.workflow.details;
 
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 import com.nirmata.workflow.details.internalmodels.RunnableTaskDagEntryModel;
 import com.nirmata.workflow.details.internalmodels.RunnableTaskDagModel;
 import com.nirmata.workflow.models.TaskDagModel;
 import com.nirmata.workflow.models.TaskId;
+import com.nirmata.workflow.spi.JsonSerializer;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.traverse.TopologicalOrderIterator;
+import java.io.File;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,24 +20,16 @@ public class RunnableTaskDagBuilder
 {
     private final TaskDagModel taskDag;
 
-    private static class TaskIdEdge extends DefaultEdge
+    public static void main(String[] args) throws Exception
     {
-        private final TaskId parentTaskId;
+        String json = Files.toString(new File("/Users/jordanzimmerman/dev/nirmata/workflow/src/test/resources/big_task_dag.json"), Charset.defaultCharset());
+        TaskDagModel taskDag = JsonSerializer.getTaskDag(JsonSerializer.fromString(json));
 
-        public TaskIdEdge(TaskId parentTaskId)
+        RunnableTaskDagModel runnableTaskDag = new RunnableTaskDagBuilder(taskDag).build();
+        for ( RunnableTaskDagEntryModel entry : runnableTaskDag.getEntries() )
         {
-            this.parentTaskId = parentTaskId;
-        }
-
-        public TaskId getParentTaskId()
-        {
-            return parentTaskId;
-        }
-
-        @Override
-        public String toString()
-        {
-            return parentTaskId.getId();
+            System.out.println(entry.getTaskId().getId());
+            System.out.println("\t" + entry.getDependencies());
         }
     }
 
@@ -44,31 +40,31 @@ public class RunnableTaskDagBuilder
 
     public RunnableTaskDagModel build()
     {
-        DefaultDirectedGraph<TaskId, TaskIdEdge> graph = new DefaultDirectedGraph<>(TaskIdEdge.class);
+        DefaultDirectedGraph<TaskId, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
         worker(graph, taskDag, null);
 
         List<RunnableTaskDagEntryModel> entries = Lists.newArrayList();
-        TopologicalOrderIterator<TaskId, TaskIdEdge> orderIterator = new TopologicalOrderIterator(graph);
+        TopologicalOrderIterator<TaskId, DefaultEdge> orderIterator = new TopologicalOrderIterator(graph);
         while ( orderIterator.hasNext() )
         {
             TaskId taskId = orderIterator.next();
-            Set<TaskIdEdge> taskIdEdges = graph.edgesOf(taskId);
+            Set<DefaultEdge> taskIdEdges = graph.edgesOf(taskId);
             Set<TaskId> processed = taskIdEdges
                 .stream()
-                .filter(edge -> !edge.getParentTaskId().equals(taskId) && !edge.getParentTaskId().getId().equals(""))
-                .map(TaskIdEdge::getParentTaskId)
+                .map(graph::getEdgeSource)
+                .filter(edge -> !edge.equals(taskId) && !edge.getId().equals(""))
                 .collect(Collectors.toSet());
             entries.add(new RunnableTaskDagEntryModel(taskId, processed));
         }
         return new RunnableTaskDagModel(entries);
     }
 
-    private static void worker(DefaultDirectedGraph<TaskId, TaskIdEdge> graph, TaskDagModel taskDag, TaskId parentId)
+    private static void worker(DefaultDirectedGraph<TaskId, DefaultEdge> graph, TaskDagModel taskDag, TaskId parentId)
     {
         graph.addVertex(taskDag.getTaskId());
         if ( parentId != null )
         {
-            graph.addEdge(parentId, taskDag.getTaskId(), new TaskIdEdge(parentId));
+            graph.addEdge(parentId, taskDag.getTaskId());
         }
         taskDag.getChildren().forEach(child -> worker(graph, child, taskDag.getTaskId()));
     }
